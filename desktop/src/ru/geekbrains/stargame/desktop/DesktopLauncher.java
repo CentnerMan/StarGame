bc6e04de74d2828c0eca3b0c267333ae4d031e8b
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2019-03-13 21:58:49 +0300

    Game with background and chip after change coordinate system.

--- desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/stargame/desktop/DesktopLauncher.java
@@ -3,16 +3,17 @@ package ru.geekbrains.stargame.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import ru.geekbrains.stargame.StarGame;
+import ru.geekbrains.stargame.Star2DGame;
+
 
 public class DesktopLauncher {
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 
-        config.title = "StarGame version 0.2";
+        config.title = "StarGame version 0.25";
         config.width = 800;
         config.height = 600;
 
-        new LwjglApplication(new StarGame(), config);
+        new LwjglApplication(new Star2DGame(), config);
     }
 }
